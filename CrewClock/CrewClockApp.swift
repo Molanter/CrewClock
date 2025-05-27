53f89b60deec3292abab6bba476c9652f2d9b2e2
Molanter <e.yarmolatiy@gmail.com>
2025-05-27 11:26:02 -0500

    Initial Commit

---
+++ CrewClock/CrewClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrewClockApp.swift
+//  CrewClock
+//
+//  Created by Edgars Yarmolatiy on 5/27/25.
+//
+
+import SwiftUI
+
+@main
+struct CrewClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
